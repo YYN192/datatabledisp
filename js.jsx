c1fad7ac2ffda41649956c4d2a590100bd175e37
YYN192 <110526560+YYN192@users.noreply.github.com>
2025-02-15 22:00:34 +0200

    Initial commit

--- js.jsx
+++ js.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1>My Image</h1>
-      <Image src="/my-image.png" alt="Optimized Image" width={300} height={300} />
+      <Image src="/datatable.jpg" alt="Optimized Image" width={300} height={300} />
     </div>
   );
 }
